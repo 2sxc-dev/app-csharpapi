3b9d36a74ae4e019297028a45943f55ff643b655
iJungleboy <daniel.mettler@2sic.com>
2026-06-30 11:02:21 +0200

    improve namespace display

--- AppCode/Data/RuleNamespace.cs
+++ AppCode/Data/RuleNamespace.cs
@@ -1,20 +1,11 @@
-using ToSic.Sxc.Data;
-
 namespace AppCode.Data
 {
-  public partial class RuleNamespace //: Custom.Data.Item16
+  public partial class RuleNamespace
   {
-    // public RuleNamespace(ITypedItem item, bool shared = false): base(item) {
-    //   SharedRule = shared;
-    // }
     public RuleNamespace Setup(bool shared = false) {
       SharedRule = shared;
       return this;
     }
-
-    // public bool IgnoreAll => Bool();
-
-    // public bool IgnoreTypeMembers => Bool();
 
     public bool SharedRule { get; private set; }
   }
